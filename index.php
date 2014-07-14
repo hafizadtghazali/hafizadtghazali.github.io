c446678f9563ae1aae578ca7cb500dad33b4cbe1
hafizadtghazahazali <me@hafizadt.ghazali.tv>
2014-07-14 10:00:50 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta charset="utf-8">
+    <title>Hafizadt Ghazali ~ Web Designer</title>
+    <meta name="description" content="I'm Web Designer living in Malaysia who has worked on projects for some of clients.">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+<header class="vcard">
+    <hgroup>
+        <h1 class="fn">Hafizadt Ghazali</h1>
+        <h2 class="role">Web Designer</h2>
+    </hgroup>
+
+    <dl>
+        <dt>Website</dt>
+            <dd><a class="url" href="http://hafizadt.ghazali.tv/">http://hafizadt.ghazali.tv/</a></dd>
+            <dd><a class="url" href="http://portfolio.hafizadt.ghazali.tv/">http://portfolio.hafizadt.ghazali.tv/</a></dd>
+      	<dt>Email</dt>
+            <dd><a class="email" href="mailto:me@hafizadt.ghazali.tv">me@hafizadt.ghazali.tv</a></dd>
+        <dt>Phone</dt>
+            <dd>+60 (12) 230-3983</dd>
+        <dt>Address</dt>
+            <dd>Kuala Lumpur, Malaysia 60000</dd>
+  	</dl>
+</header>
+
+<section>
+    <?php
+        $year_diff = date("Y") - 1984;
+        $month_diff = date("m") - 3;
+        $day_diff = date("d") - 10;
+        if ($month_diff < 0)
+          $year_diff--;
+        elseif ($month_diff == 0 && $day_diff < 0)
+          $year_diff--;
+    ?>
+    <h1>About me</h1>
+    <p>I'm <?php print $year_diff; ?> years old Interactive Designer living in Malaysia who has worked on projects for some of clients. Based on my experience in the field, my expected salary would be over <strong>RM9000</strong>, negotiable.</p>
+</section>
+
+<section>
+    <h1>Expertise</h1>
+
+    <section>
+        <header>
+            <hgroup>
+                <h1>Capabilities</h1>
+            </hgroup>
+        </header>
+        <ul>
+            <li>Development for web sites and applications.</li>
+            <li>Site planning.</li>
+            <li>User interface design.</li>
+            <li>Development for interactive-based art.</li>
+            <li>Interaction design (IxD).</li>
+        </ul>
+    </section>
+
+    <section>
+        <header>
+            <hgroup>
+                <h1>Technical skills</h1>
+            </hgroup>
+        </header>
+        <ul>
+            <li>Able to hand-coded HTML, CSS, and Actionscript.</li>
+            <li>Development for Magento, Wordpress open source platforms.</li>
+            <li>Experience working in PHP environments.</li>
+            <li>Experience working with physical computing .</li>
+            <li>Development for Processing.</li>
+        </ul>
+  </section>
+
+</section>
+
+<section>
+    <h1>Experience</h1>
+
+    <section>
+        <header>
+            <hgroup>
+                <h1>Interactive developer</h1>
+                <h2>&nbsp;</h2>
+            </hgroup>
+            <p>in Penang June 2011–July 2011</p>
+        </header>
+        <p>Applied actionscript to create an interactive installation. Handled maintenance and development for various installation projects.</p>
+    </section>
+
+    <section>
+        <header>
+            <hgroup>
+                <h1>Freelance web developer</h1>
+                <h2>&nbsp;</h2>
+        </hgroup>
+            <p>December 2011–March 2012</p>
+        </header>
+        <p>Front-end developer for <a href="http://daminteractive.com/">DAM+Interactive</a><a href="http://daminteractive.com/"></a></p>
+    </section>
+
+    <section>
+        <header>
+            <hgroup>
+                <h1>Web cum Graphic designer</h1>
+                <h2>&nbsp;</h2>
+        </hgroup>
+            <p>December 2011–April 2012</p>
+        </header>
+        <p>Working on various projects, site redesign and printed-layout.</p>
+    </section>
+
+    <section>
+    <header>
+      <hgroup>
+                <h1>Web designer</h1>
+                <h2>&nbsp;</h2>
+      </hgroup>
+        <p>in MSD Intelligence June 2012–now</p></header>
+        <p>Working on various projects and site redesign.</p>
+    </section>
+
+</section>
+
+<section>
+    <h1>Work</h1>
+    <ul>
+        <li>Ilham Echenta~ E-commerce Website(2014) / <a rel="external nofollow" href="http://ilhamechenta.com">http://ilhamechenta.com</a></li>
+        <li>Portal Kotabuku~ Website(2013) / <a rel="external nofollow" href="http://kotabuku.my">http://kotabuku.my</a></li>
+		<li>Stylvista~ Website(2013) / <a rel="external nofollow" href="http://stylvista.com">http://stylvista.com</a></li>
+        <li>DAM+Interactive ~ Website(2012) / <a rel="external nofollow" href="http://daminteractive.com/">http://daminteractive.com</a></li>
+		<li>Door to Another Time ~Installation (2011) / <a rel="external nofollow" href="http://www.dacfestival.com/installation.htm">http://www.dacfestival.com/installation</a></li>
+        <li>this is stairscape ~ Installation (2011) / <a rel="external nofollow" href="http://apasajayangkaumahu.wordpress.com/">http://apasajayangkaumahu.wordpress.com</a></li>
+    </ul>
+</section>
+
+<section>
+    <h1>Articles And Honors</h1>
+    <section>
+        <ul>
+            <li>Featured as one of the collaborator for DA+C Festival published in 2012 - <a href="http://hafizadt.ghazali.tv/vitae/doc/dac-festival-screenshot.jpg">http://hafizadt.ghazali.tv/vitae/doc/dac-festival-screenshot.jpg</a></li>
+            <li>Google's SME Stimulus Package - <a href="http://hafizadt.ghazali.tv/vitae/doc/perridot-google.pdf">http://hafizadt.ghazali.tv/vitae/doc/perridot-google.pdf</a></li>
+        </ul>
+    </section>
+</section>
+
+<section>
+    <h1>Education</h1>
+    <section>
+        <hgroup>
+            <h1>Multimedia University</h1>
+            <h2>BSc, Digital Media</h2>
+        </hgroup>
+        <p>2007–2011</p>
+    </section>
+    <section>
+         <hgroup>
+            <h1>International University College of Technology Twintech</h1>
+            <h2>Electronic Engineering</h2>
+        </hgroup>
+        <p>2002–2006</p>
+    </section>
+</section>
+
+<section>
+    <h1>Referee</h1>
+    <section>
+        <hgroup>
+            <h1>Lim Kok Yoong</h1>
+            <h2>Lecturer and Head of Digital Media, Multimedia University</h2>
+        </hgroup>
+        <p><a href="http://cargocollective.com/limkokyoong" target="_blank">http://cargocollective.com/limkokyoong</a></p>
+    </section>
+</section>
+
+<section class="contact">
+    <ul>
+        <li><a rel="external" href="http://twitter.com/hafizadtghazali">Twitter</a></li>
+        <li><a href="mailto:me@hafizadt.ghazali.tv">Email</a></li>
+    </ul>
+</section>
+
+</body></html>
